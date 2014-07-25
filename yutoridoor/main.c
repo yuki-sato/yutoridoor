93bc37652bf02dfa1f17dfc598ddcff927f26f79
yuki-sato.com <yuki@yuki-sato.com>
2014-07-25 23:36:16 +0900

    Initial Commit

---
+++ yutoridoor/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  yutoridoor
+//
+//  Created by oyabunn on 2014/07/25.
+//  Copyright (c) 2014年 yuki-sato. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
