074f6666afef1c62c98a7aa91a0a39253479e691
yuki-sato.com <yuki@yuki-sato.com>
2014-07-25 23:46:50 +0900

    init

---
+++ main.c
@@ -0,0 +1,39 @@
+//
+//  main.c
+//  yutoridoor
+//
+//  Created by oyabunn on 2014/07/25.
+//  Copyright (c) 2014年 yuki-sato. All rights reserved.
+//
+
+#include "cweb/cweb.h"
+
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    CWebListen(80);
+    return 1;
+}
+
+void didReceiveCWebRequest(CWebTCPConnection *connection, CWebHTTPRequest *request)
+{
+    CWebHTTPResponse *response=NULL;
+    char *html = NULL;
+    if(CWebRequestMatch(request, "GET", "/")){
+        CWebObject *obj = CWebObjectCreateDictionaryStringValueWithCopy("title", "/");
+        html = CWebRenderHTML("./index.html", obj);
+        CWebObjectFree(obj);
+        response = CWebResponseCreateWithHTMLBODY(&html);
+        
+    }else{
+        CWebObject *obj = CWebObjectCreateDictionaryStringValueWithCopy("title", "404 Notfound");
+        html = CWebRenderHTML("./index.html", obj);
+        response = CWebResponseCreateWithHTMLBODY(&html);
+        CWebObjectFree(obj);
+        response->statusCode = 404;
+    }
+    CWebResponse(connection, response);
+    CWebResponseFree(response);
+}
---
+++ makefile
@@ -0,0 +1,3 @@
+# simple
+yutoridoor: cweb/cweb_object.c cweb/cweb_http.c cweb/cweb_render.c cweb/cweb_main.c main.c
+	gcc -Wall -o yutoridoor cweb/cweb_object.c cweb/cweb_http.c cweb/cweb_render.c cweb/cweb_main.c main.c
